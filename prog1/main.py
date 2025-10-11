e96dc8dc76cb1ccdc1f7837d3ebcac44c59596d0
jhonmena08 <jhonmenahurtado@gmail.com>
2025-10-11 08:41:05 -0500

    agregar dos columnas

--- prog1/main.py
+++ prog1/main.py
@@ -5,17 +5,31 @@
 # ]
 # ///
 
+import os
 import polars as pl # type: ignore
 
 
 def main() -> None:
+    os.system('cls')
+
     df = pl.read_csv('C:/Users/Jhonm/repos/scripts/prog1/datos.csv')
-    
-    df2 = df.filter(
-        pl.col('kilometraje') < 50_000
+
+    # convertir columna a Float
+    df = df.with_columns(
+        pl.col('price').cast(pl.Float64)
     )
 
-    print(df2)
+    df = df.with_columns(
+        pl.col('product').str.split(' ').list.get(0).alias('marca'),
+        pl.col('product').str.split(' ').list.get(1).alias('linea')
+    )
+
+    print(df.head(5))
+
+    # print(df.select(
+    #     pl.col('product')
+    # ).unique())
+
 
 
 if __name__ == "__main__":
