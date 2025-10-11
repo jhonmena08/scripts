cb2c009ec32d058d2d11611a8e904e6ca1c12f4f
jhonmena08 <jhonmenahurtado@gmail.com>
2025-10-11 11:08:20 -0500

    nuevo proyecto prog2

---
+++ prog2/main.py
@@ -0,0 +1,14 @@
+from myfunciones import *
+
+
+def main():
+    # print(sumar(10, 20))
+    # print(sumar2(10, 20))
+    # mostrar_name()
+
+    # tipos_datos_python()
+
+    ciclo_white()
+
+if __name__ == "__main__":
+    main()
