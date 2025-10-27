a1a7ddbf209de736f9c42bcce0026c7dc35ce243
Jhon <jhonmena08>
2025-10-26 20:52:12 -0500

    paquete topo v1.0.5 con pandas

--- prog/src/main.py
+++ prog/src/main.py
@@ -1,17 +1,30 @@
-from topo.cogo import Point, distancia
+from topo.cogo import Point, acimut_dist
 import pandas as pd
 import os
 
 
+def get_point(row: tuple) -> Point:
+    return Point(row['x'], row['y'])
+
+
+
 def main() -> int:
     os.system('cls')
-    
-    df = pd.DataFrame({
-        'location': [Point(100, 200), Point(500,456), Point(199, 87, 'cer'), Point(344.52, 233.04)]
-    })
 
-    print(df)
+    df = pd.read_csv('ptos.csv')
+    puntos = [fila for fila in df.apply(get_point, axis=1)]
 
+
+    # df2 = pd.DataFrame({
+    #     'location': puntos
+    # })
+
+    # calcular acimut y distancia
+    for idx in range(len(puntos) - 1):
+        print(f'{acimut_dist(puntos[idx],  puntos[idx + 1])}\t{puntos[idx].descripcion} -> {puntos[idx + 1].descripcion}')
+
+
+    print()
     return 0
 
 
