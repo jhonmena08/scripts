164d20ad6e0c00c55a8a44059e1c4b2e74e6efb9
Jhon <jhonmena08>
2025-11-03 13:23:38 -0500

    modificacion main transformaciones

--- transformacion/main.py
+++ transformacion/main.py
@@ -18,14 +18,13 @@ def transformar(origen: str, dest: str, lat: float, lon: float, h: float) -> tup
 
 def main() -> int:
 
-    # Magna-Cali -> CTM12
+    # magna-cali -> ctm12
     # x, y, z = transformar('EPSG:6249', 'EPSG:9377', 1061912.027, 862535.415, 0.00)
 
-    # Geocentricas wgs84 -> Magna-Cali
-    x, y, z = transformar('EPSG:4328', 'EPSG:6249', 1483099.97741, -6193060.17470, 373124.22773)
+    # geocentricas wgs84 -> magna-cali
+    x, y, z = transformar('EPSG:4328', 'EPSG:6249', 1484438.02386, -6192868.80262, 370607.90097)
 
     print(f'x:{x}, y:{y}, z:{z}')
-
     return 0
 
 
