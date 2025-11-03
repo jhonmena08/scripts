5b4162654b469742f6ee15b9ce7c8ba2e9862309
Jhon <jhonmena08>
2025-11-03 11:44:18 -0500

    modificacion proyecto transformacion

--- transformacion/main.py
+++ transformacion/main.py
@@ -5,19 +5,29 @@
 # ]
 # ///
 
-from pyproj import Transformer
+from pyproj import Transformer # type: ignore
+
+
+
+def transformar(origen: str, dest: str, lat: float, lon: float, h: float) -> tuple[float, float]:
+    tf = Transformer.from_crs(origen, dest, always_xy=True )
+    x, y, z = tf.transform(lat, lon, h)
+    return (x, y, z)
+
 
 
 def main() -> int:
-    # Magna-Cali -> CTM12
-    tf = Transformer.from_crs('EPSG:6249', 'EPSG:9377', always_xy=True )
-    
-    e, n = 1061912.027, 862535.415
 
-    x, y = tf.transform(e, n)
-    print(f'x:{x}, y:{y}')
+    # Magna-Cali -> CTM12
+    # x, y, z = transformar('EPSG:6249', 'EPSG:9377', 1061912.027, 862535.415, 0.00)
+
+    # Geocentricas wgs84 -> Magna-Cali
+    x, y, z = transformar('EPSG:4328', 'EPSG:6249', 1483099.97741, -6193060.17470, 373124.22773)
+
+    print(f'x:{x}, y:{y}, z:{z}')
 
     return 0
+
 
 
 if __name__ == '__main__':
