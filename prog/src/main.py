d0634a0a1e96e783a7758f9d8b5422aa61064be4
Jhon <jhonmena08>
2025-11-24 15:44:39 -0500

    delete project transformacion

--- prog/src/main.py
+++ prog/src/main.py
@@ -1,11 +1,43 @@
-from topo.cogo import Point, acimut_dist
+from pyproj import Transformer
+from libtopo.cogo import Point, acimut_dist
 import pandas as pd
 import os
+
+
+
+def transformar(origen: str, dest: str, lat: float, lon: float, h: float) -> tuple[float, float]:
+    tf = Transformer.from_crs(origen, dest, always_xy=True)
+    x, y, z = tf.transform(lat, lon, h)
+    return (x, y, z)
+
+# end
+
+
+def get_coord_ctm12(puntos: list[Point]) -> None:
+    for p in puntos:
+        # Magna Cali -> WGS84
+        x, y, z = transformar(
+        "EPSG:6249", "EPSG:4326", p.x, p.y, 0.0)
+
+        print(f'|{x:^15.3f}|{y:^15.3f}|{p.descripcion:^15}|')
+
+# end
+
 
 
 def get_point(row: tuple) -> Point:
     return Point(row['E1'], row['N1'], row['vertice'])
 
+# end
+
+
+def calcular_acimut_dist(puntos: list[Point]) -> None:
+    for idx in range(len(puntos) - 1):
+        az_dist: tuple[str, float] =  acimut_dist(puntos[idx],  puntos[idx + 1])
+        vertices: str = str(puntos[idx].descripcion) + ' -> ' + str(puntos[idx + 1].descripcion)
+        print(f'|{az_dist[0]:^12}|{az_dist[1]:10.3f}|{vertices:^25}|')
+
+# end
 
 
 def main() -> int:
@@ -14,19 +46,14 @@ def main() -> int:
     df = pd.read_excel('ptos_control_cali.xlsx')
 
     # filtrar
-    # df = df[df['ubicacion'].str.contains('autonoma')][['N1', 'E1', 'vertice']]
-    puntos = [fila for fila in df.apply(get_point, axis=1)]
+    df = df[df['ubicacion'].str.contains('autonoma')][['N1', 'E1', 'vertice']]
+    ptos = [fila for fila in df.apply(get_point, axis=1)]
 
-
-    # calcular y mostrar acimut y distancia
-    for idx in range(len(puntos) - 1):
-        az_dist: tuple[str, float] =  acimut_dist(puntos[idx],  puntos[idx + 1])
-        vertices: str = str(puntos[idx].descripcion) + ' -> ' + str(puntos[idx + 1].descripcion)
-
-        print(f'|{az_dist[0]:^12}|{az_dist[1]:10.3f}|{vertices:^25}|')
-
+    # calcular_acimut_dist(ptos)
+    get_coord_ctm12(ptos)
     print()
     return 0
+
 
 
 if __name__ == "__main__":
